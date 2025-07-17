798d297d89d023a60dfff7a4406cab7d859d3515
Moise1 <rwibutso.mo@gmail.com>
2025-07-17 17:17:49 +0200

    fix missing module issue

--- client/src/app/(dashboard)/[role]/page.tsx
+++ client/src/app/(dashboard)/[role]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
+import React from "react";
 import { useParams } from "next/navigation";
 import ReceptionistDashBoard from "./(receptionist)/receptionist-dashboard";
 
 export default function Home() {
   const { role } = useParams<{ role: string }>();
 
-  const dashboard: Record<string, () => JSX.Element> = {
+  const dashboard: Record<string, () => React.JSX.Element> = {
     receptionist: ReceptionistDashBoard,
    
   };
