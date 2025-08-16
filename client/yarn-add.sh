e88464c2884f591942b992e21bc6ea5847920835
Moise1 <rwibutso.mo@gmail.com>
2025-08-16 19:15:34 +0200

    edit config

--- client/yarn-add.sh
+++ client/yarn-add.sh
@@ -2,13 +2,19 @@
 
 if [[ $1 == "--yarn" ]]; then
     shift
-    docker compose -f ../docker-compose.dev.yml exec client yarn add "$@"
+    # Check if -D flag is present
+    if [[ "$1" == "-D" ]]; then
+        shift
+        docker compose -f ../docker-compose.dev.yml exec client yarn add -D "$@"
+    else
+        docker compose -f ../docker-compose.dev.yml exec client yarn add "$@"
+    fi
 elif [[ $1 == "--shadcn" ]]; then
     shift
     docker compose -f ../docker-compose.dev.yml exec client npx shadcn@latest add "$@"
 else
     echo "Usage:"
-    echo "  $0 --yarn <package-name>"
+    echo "  $0 --yarn [-D] <package-name>"
     echo "  $0 --shadcn <package-name>"
     exit 1
 fi
