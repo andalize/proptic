ffa99114a7664244c5d46d0de5a04ee25f949bd9
Moise1 <rwibutso.mo@gmail.com>
2025-08-16 18:23:37 +0200

    edit config file

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   experimental: {
-    // @ts-ignore - This might not be in types yet
-    errorOverlay: false,
   },
 };
 
